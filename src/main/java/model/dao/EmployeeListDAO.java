065c90d6c4ee3f807683c7a6f02b1e6a55826b0e
comsize-jonuon-chon <jonuon.chon@comsize.com>
2023-10-26 12:02:22 +0900

    2023/10/26　11:59 JST時点

--- src/main/java/model/dao/EmployeeListDAO.java
+++ src/main/java/model/dao/EmployeeListDAO.java
@@ -1,6 +1,68 @@
 package model.dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import model.entity.EmployeeListBean;
+
 public class EmployeeListDAO {
-	
-	
+
+	public List<EmployeeListBean> selectEmployee() throws SQLException, ClassNotFoundException {
+
+		/* インスタンス化して、オブジェクトとして従業員リストを生成します。*/
+		List<EmployeeListBean> employeeList = new ArrayList<EmployeeListBean>();
+
+		/* 従業員マスタ、m_employeeのデータを全てSELECTします。*/
+		String sql = " SELECT * FROM m_employee";
+
+		/* データベース接続を行い、同時にpreparedstatementの取得を行います。*/
+		try (Connection con = ConnectionManager.getConnection();
+				PreparedStatement pstmt = con.prepareStatement(sql)) {
+
+			/* SQLステートメントを実行します。*/
+			ResultSet res = pstmt.executeQuery();
+
+			/* 従業員マスタ、m_employeeに登録されている情報が全て取り出されるまで、結果の操作を行います。*/
+			while(res.next()) {
+				
+				/* インスタンス化して、EmployeeListBeanを生成します。*/
+				EmployeeListBean bean = new EmployeeListBean ();
+				
+				String employeeCode = res.getString("employee_code");
+				String lastName = res.getString("last_name");
+				String firstName = res.getString("first_name");
+				String lastKanaName = res.getString("last_kana_name");
+				String firstKanaName = res.getString("first_kana_name");
+				String gender = res.getString("gender");
+				int birthDay = res.getInt("birth_day");
+				String sectionCode = res.getString ("section_code");
+				int hireDate = res.getInt("hire_date");
+				int updateDatetime = res.getInt("update_datetime");
+				
+				bean.setEmployeeCode(employeeCode);
+				bean.setLastName(lastName);
+				bean.setFirstName(firstName);
+				bean.setLastKanaName(lastKanaName);
+				bean.setFirstKanaName(firstKanaName);
+				bean.setGender(gender);
+				bean.setBirthDay(birthDay);
+				bean.setSectionCode(sectionCode);
+				bean.setHireDate(hireDate);
+				bean.setUpdateDatetime(updateDatetime);
+				
+				employeeList.add(bean);
+				
+				
+			}
+				/* employeeListを返します。*/
+				return employeeList;
+			
+		}
+
+	}
+
 }
--- src/main/java/servlet/EmployeeListServlet.java
+++ src/main/java/servlet/EmployeeListServlet.java
@@ -1,31 +1,39 @@
 package servlet;
 
 import java.io.IOException;
+import java.sql.SQLException;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import model.dao.EmployeeListDAO;
+import model.entity.EmployeeListBean;
+
 /**
- * Servlet implementation class EmployeeRegistrationServlet
+ * Servlet implementation class EmployeeListServlet
  */
-@WebServlet("/employee-registration-servlet")
+@WebServlet("/employee-list-servlet")
 public class EmployeeListServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public EmployeeListServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public EmployeeListServlet() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
@@ -33,9 +41,34 @@ public class EmployeeListServlet extends HttpServlet {
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+
+	/* 従業員リストの表示は、こちらのdoPOSTメソッドで行います。*/
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		
+		/* エンコーディングでUTF-8を指定して、文字化けを防ぎます。*/
+		request.setCharacterEncoding("UTF-8");
+
+		/* インスタンス化して、EmployeeListDAOを生成します。*/
+		EmployeeListDAO dao = new EmployeeListDAO ();
+		
+		/* EmployeeListDAOでthrowされた例外を、EmployeeListServletでcatchします。*/
+		try {
+			/* EmployeeListDAOを使用します。*/
+			List <EmployeeListBean> employeeList = dao.selectEmployee();
+			
+			/*リクエストスコープをセットします。*/
+			request.setAttribute("employeeList", employeeList);
+			
+		} catch (ClassNotFoundException | SQLException |NullPointerException e) {
+			e.printStackTrace();
+			
+		}
+		/* 最後にリクエストのjspへの転送を行います。*/
+		/* これにより、従業員情報一覧画面はEmployeelistServletのフォワードになります。*/
+		RequestDispatcher rd = request.getRequestDispatcher("EmployeeList.jsp");
+		rd.forward(request, response);
+		
 	}
 
 }
